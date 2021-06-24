93ec555882e6cf28b5ec9dce96e6b49b1db64d59
Alexvjunior <alexsandervieirajunior@hotmail.com>
2021-06-24 19:34:35 -0300

    Add files via upload

---
+++ funcoes.py
@@ -0,0 +1,13 @@
+lista = [
+    "NOME_FUNCIONARIO",
+    "CARGO",
+    "CATEGORIA",
+    "NOME_UNIDADE",
+    "JORNADA",
+    "SITUAÇÃO"
+]
+
+def to_upper_case(arquivo):
+    for l in lista:
+        arquivo[l] = arquivo[l].str.upper()
+    return arquivo
